91d7b60ab89fd9add6babe52b23e655e38f04fde
szamarripa <szamarripa@s2grupo.es>
2015-01-07 11:26:44 +0100

    Adding Sensor App Event class

---
+++ src/main/java/eu/musesproject/server/eventprocessor/correlator/model/owl/SensorAppEvent.java
@@ -0,0 +1,76 @@
+package eu.musesproject.server.eventprocessor.correlator.model.owl;
+
+/*
+ * #%L
+ * MUSES Server
+ * %%
+ * Copyright (C) 2013 - 2014 S2 Grupo
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+public class SensorAppEvent extends Event {
+	
+	private int id;
+	private String backgroundProcess;
+	private String appName;
+	private String appVersion;
+	private String packageName;
+	private long timestamp;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public long getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getBackgroundProcess() {
+		return backgroundProcess;
+	}
+	public void setBackgroundProcess(String backgroundProcess) {
+		this.backgroundProcess = backgroundProcess;
+	}
+	public String getAppName() {
+		return appName;
+	}
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+	public String getAppVersion() {
+		return appVersion;
+	}
+	public void setAppVersion(String appVersion) {
+		this.appVersion = appVersion;
+	}
+	public String getPackageName() {
+		return packageName;
+	}
+	public void setPackageName(String packageName) {
+		this.packageName = packageName;
+	}
+
+
+	
+
+	
+	
+
+}
