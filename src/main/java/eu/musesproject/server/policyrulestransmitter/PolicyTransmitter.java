6038e0d18bc6b66801ad345e7270ecf1e59c57bf
szamarripa <szamarripa@s2grupo.es>
2015-03-17 16:47:11 +0100

    Sometimes MUSES is sending null data from server to client

--- src/main/java/eu/musesproject/server/policyrulestransmitter/PolicyTransmitter.java
+++ src/main/java/eu/musesproject/server/policyrulestransmitter/PolicyTransmitter.java
@@ -91,7 +91,10 @@ public class PolicyTransmitter {
 		//logger.log(Level.INFO, "[PolicyTransmitter.sendData]");
 		//logger.log(Level.INFO, "		PolicyTransmitter sendData. Data to be sent:"+dataToSend);
 		logger.log(Level.INFO, "		Sending data with sessionId:"+sessionId);
-		connManager.sendData(sessionId, dataToSend);
+		if (dataToSend != null){
+			connManager.sendData(sessionId, dataToSend);
+		}
+		
 	}
 
 }
