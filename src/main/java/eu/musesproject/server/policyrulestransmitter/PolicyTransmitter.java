e75a593c858a21b6cf78d237affc65af7a7ecd83
Yasir Ali <yasir.ali@swedenconnectivity.com>
2015-03-23 12:55:19 +0100

    if check uncommented after fixing the JSON problem.
    if check uncommented after fixing the JSON problem.

--- src/main/java/eu/musesproject/server/policyrulestransmitter/PolicyTransmitter.java
+++ src/main/java/eu/musesproject/server/policyrulestransmitter/PolicyTransmitter.java
@@ -91,10 +91,9 @@ public class PolicyTransmitter {
 		//logger.log(Level.INFO, "[PolicyTransmitter.sendData]");
 		//logger.log(Level.INFO, "		PolicyTransmitter sendData. Data to be sent:"+dataToSend);
 		logger.log(Level.INFO, "		Sending data with sessionId:"+sessionId);
-		connManager.sendData(sessionId, dataToSend);
-//		if (dataToSend != null){   // temporarily commented  
-//			connManager.sendData(sessionId, dataToSend);
-//		}
+		if (dataToSend != null){
+			connManager.sendData(sessionId, dataToSend);
+		}
 		
 	}
 
