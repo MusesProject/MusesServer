07114a2cfbd3ef07cf6ba0ad774610cbd483c39e
xaviertiti <xavier.titi@gmail.com>
2015-04-07 15:18:28 +0200

    -Adding new table for storing trust data for each decision

---
+++ src/main/java/eu/musesproject/server/entity/DecisionTrustvalues.java
@@ -0,0 +1,64 @@
+package musesdatabase;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the decision_trustvalues database table.
+ * 
+ */
+@Entity
+@Table(name="decision_trustvalues")
+@NamedQuery(name="DecisionTrustvalues.findAll", query="SELECT d FROM DecisionTrustvalues d")
+public class DecisionTrustvalues implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="decision_trustvalue_id")
+	private int decisionTrustvalueId;
+
+	@Column(name="decision_id")
+	private int decisionId;
+
+	private double devicetrustvalue;
+
+	private double usertrustvalue;
+
+	public DecisionTrustvalues() {
+	}
+
+	public int getDecisionTrustvalueId() {
+		return this.decisionTrustvalueId;
+	}
+
+	public void setDecisionTrustvalueId(int decisionTrustvalueId) {
+		this.decisionTrustvalueId = decisionTrustvalueId;
+	}
+
+	public int getDecisionId() {
+		return this.decisionId;
+	}
+
+	public void setDecisionId(int decisionId) {
+		this.decisionId = decisionId;
+	}
+
+	public double getDevicetrustvalue() {
+		return this.devicetrustvalue;
+	}
+
+	public void setDevicetrustvalue(double devicetrustvalue) {
+		this.devicetrustvalue = devicetrustvalue;
+	}
+
+	public double getUsertrustvalue() {
+		return this.usertrustvalue;
+	}
+
+	public void setUsertrustvalue(double usertrustvalue) {
+		this.usertrustvalue = usertrustvalue;
+	}
+
+}
